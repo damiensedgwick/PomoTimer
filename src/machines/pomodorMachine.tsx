d42852912df703e0f39ce50c2286733c4fb0ab0e
Damien Sedgwick <damiensedgwick@icloud.com>
2021-04-10 22:10:00 +0100

    Amended pomodoroMachine for simple states

--- src/machines/pomodorMachine.tsx
+++ src/machines/pomodorMachine.tsx
@@ -3,5 +3,19 @@ import {Machine} from 'xstate';
 export const pomodoroMachine = Machine({
   id: 'pomodoro',
   initial: 'idle',
-  states: {},
+  states: {
+    idle: {
+      on: {
+        START_TIMER: 'timer_started',
+      },
+    },
+    timer_started: {
+      after: {
+        1.5e6: 'idle',
+      },
+      on: {
+        PAUSE_TIMER: 'idle',
+      },
+    },
+  },
 });
